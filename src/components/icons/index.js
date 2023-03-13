a267871c4b677bb7e6910af79f7baf5ad78f6893
redskull422 <kofifa.pg@gmail.com>
2023-03-13 14:12:39 +0330

    Update : New Icons Added TO the Project

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -3,7 +3,9 @@ import iFilter from './iFilter.vue';
 import iSetting from './iSetting.vue';
 import iBell from './iBell.vue';
 import iHeart from './iHeart.vue';
-import iSwitch from './iswitch.vue'
+import iClutch from './iClutch.vue';
+import iSwitch from './iswitch.vue';
+import iUser from './iUser.vue'
 
 
-export {iSearch , iFilter , iSetting, iBell, iHeart,iSwitch}
+export {iSearch , iFilter , iSetting, iBell, iHeart,iSwitch,iClutch,iUser}
