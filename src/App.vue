2d990301c56d58919fddadc364c460243e24b8f0
redskull422 <kofifa.pg@gmail.com>
2023-03-14 13:40:50 +0330

    Feature ; Footer Added

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div class="container mx-auto bg-gray-100">
     <y-header />
     <router-view />
+    <div class="col-span-12 mt-10 md:bg-white p-6"><y-footer /></div>
   </div>
 </template>
 
 <script>
 import "./tailwind.css";
 import yHeader from "@/components/header.vue";
+import YFooter from "@/components/footer.vue";
 export default {
   name: "App",
   components: {
     yHeader,
+    YFooter,
   },
 };
 </script>
